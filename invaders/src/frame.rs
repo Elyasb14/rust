6f677b055ba375089382667a3c1ad8ef80687102
Elyas Bianchi <114762087+Elyasb14@users.noreply.github.com>
2023-01-26 09:27:58 -0800

    commit

--- invaders/src/frame.rs
+++ invaders/src/frame.rs
@@ -2,10 +2,14 @@ use crate::{NUM_COLS, NUM_ROWS};
 
 pub type Frame = Vec<Vec<&'static str>>;
 
+// makes new frame, vector of vectors
 pub fn new_frame() -> Frame {
+    // make columns
     let mut cols = Vec::with_capacity(NUM_COLS);
     for _ in 0..NUM_COLS {
+        // make column desirable size
         let mut col = Vec::with_capacity(NUM_ROWS);
+        // add space to column entries
         for _ in 0..NUM_ROWS {
             col.push(" ")
         }
@@ -14,6 +18,7 @@ pub fn new_frame() -> Frame {
     cols
 }
 
+// trait to be able to be drawn onto frame
 pub trait Drawable {
     fn draw(&self, frame: &mut Frame);
 }
